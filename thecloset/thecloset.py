6a63454855a93c8d63fcba6281246d8884033194
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2020-01-27 11:32:44 -0800

    Added thecloset: A directory for generic modules.

---
+++ thecloset/thecloset.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os.path
+from pathlib import Path
+
+def getDrano():
+    print("Drano")
+
+def getClorox():
+    print("Clorox")
+
+def getRaid():
+    print("Raid")
+
+def getLysol():
+    print("Lysol")
