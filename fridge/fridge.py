e08e72943308222debbf830af07cba16fa008a08
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2020-01-27 11:33:46 -0800

    Took out the stuff that doesn't belong in the fridge.

--- fridge/fridge.py
+++ fridge/fridge.py
@@ -6,37 +6,21 @@ from pathlib import Path
 def getMilk():
     print("Milk")
 
-def getDrano():
-    print("Drano")
-
 def getEggs():
     print("Eggs")
-
-def getClorox():
-    print("Clorox")
 
 def getVegetables():
     print("Vegetables")
 
-def getRaid():
-    print("Raid")
-
 def getButter():
     print("Butter")
-
-def getLysol():
-    print("Lysol")
 
 def main():
     print("The %s contains:" % Path(os.path.basename(__file__)).stem)
     getMilk()
-    getDrano()
     getEggs()
-    getClorox()
     getVegetables()
-    getRaid()
     getButter()
-    getLysol()
 
 if __name__ == "__main__":
     main()
