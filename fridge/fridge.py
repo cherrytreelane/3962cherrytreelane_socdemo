bf82684c17615e193065c44a594d7b5757865174
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2020-01-27 10:52:27 -0800

    A messy and dangerous fridge is the same as a non modular piece of code

---
+++ fridge/fridge.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+def getMilk():
+    print("Milk")
+
+def getDrano():
+    print("Drano")
+
+def getEggs():
+    print("Eggs")
+
+def getClorox():
+    print("Clorox")
+
+def getVegetables():
+    print("Vegetables")
+
+def getRaid():
+    print("Raid")
+
+def getButter():
+    print("Butter")
+
+def getLysol():
+    print("Lysol")
+
+def main():
+    print("The fridge contains:")
+    getMilk()
+    getDrano()
+    getEggs()
+    getClorox()
+    getVegetables()
+    getRaid()
+    getButter()
+    getLysol()
+
+if __name__ == "__main__":
+    main()
