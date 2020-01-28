9ba87f5f5ef212ff607155956c60f12047bbad0e
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2020-01-27 16:29:20 -0800

    Made functions return strings

--- livingroom/livingroom.py
+++ livingroom/livingroom.py
@@ -4,15 +4,15 @@ import os.path
 from pathlib import Path
 
 def getCouch():
-    print("Couch")
+    return "Couch"
 
 def getCoffeeTable():
-    print("Coffee table")
+    return "Coffee table"
 
 def main():
     print("The %s contains:" % Path(os.path.basename(__file__)).stem)
-    getCouch()
-    getCoffeeTable()
+    print(getCouch())
+    print(getCoffeeTable())
 
 if __name__ == "__main__":
     main()
